92ccb8cb1037e02a17c07ee498c09185bffda8e6
s-davies <swdavies.contact@gmail.com>
2020-06-20 08:32:56 -0400

    Updates

---
+++ precache-manifest.262d83ffa54d9bbd4d1f94cdccd5dafd.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d26913c0793a5df2647b82110e8f188f",
+    "url": "/websites4u/index.html"
+  },
+  {
+    "revision": "9ffb0587049ae459c807",
+    "url": "/websites4u/static/css/main.331a5b38.chunk.css"
+  },
+  {
+    "revision": "7a2021a041c6c0a54aa9",
+    "url": "/websites4u/static/js/2.c2171e41.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/websites4u/static/js/2.c2171e41.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9ffb0587049ae459c807",
+    "url": "/websites4u/static/js/main.de56c821.chunk.js"
+  },
+  {
+    "revision": "909008fa210c2183fe2a",
+    "url": "/websites4u/static/js/runtime-main.fd12dea3.js"
+  },
+  {
+    "revision": "4aefeea9954987cfc3b9f344a3cc154c",
+    "url": "/websites4u/static/media/Image.4aefeea9.jpg"
+  },
+  {
+    "revision": "3a8c0d8be7fe574d48f58646c2915662",
+    "url": "/websites4u/static/media/Image4.3a8c0d8b.jpg"
+  },
+  {
+    "revision": "8199f572cfea911d3a579e605c7aded6",
+    "url": "/websites4u/static/media/Image5.8199f572.jpg"
+  }
+]);
