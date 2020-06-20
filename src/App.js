5375353d9adf07b328715ef59e86163f34eaa9b4
s-davies <swdavies.contact@gmail.com>
2020-06-20 11:20:53 -0400

    add favicon

--- src/App.js
+++ src/App.js
@@ -6,11 +6,13 @@ import Blog from './blog';
 import Navbar from './navbar';
 import Footer from './footer';
 import { Route } from 'react-router-dom';
+import Favicon from 'react-favicon';
 
 class App extends React.Component {
   render() {
     return (
       <div className="App">
+        <Favicon url="https://www.freeiconspng.com/uploads/number-4-icon-8.png" />
         <Navbar />
         <Route exact path="/" component={Home} />
         <Route exact path="/blog" component={Blog} />
